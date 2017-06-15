9d194abb291ee1396346e6101fde8cc8d9a8887f
jpd <john.dandison@live.com>
2017-06-15 17:55:08 -0400

    initial commit

--- start-json.bat
+++ start-json.bat
@@ -1,3 +1,3 @@
-start dotnet run --project D:\code2\git\AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj eventhub json
+start dotnet run --project AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj eventhub json
 timeout /t 5
-start dotnet run --project D:\code2\git\AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj iot json
+start dotnet run --project AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj iot json
--- start-protobuf.bat
+++ start-protobuf.bat
@@ -1,3 +1,3 @@
-start dotnet run --project D:\code2\git\AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj eventhub protobuf
+start dotnet run --project AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj eventhub protobuf
 timeout /t 5
-start dotnet run --project D:\code2\git\AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj iot protobuf
+start dotnet run --project AzureHttpHub\EventHubClientProcessor\EventHubClientProcessor.csproj iot protobuf
